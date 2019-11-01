1cca3b3d2f65809ddd1ac508b874dcefd8c9f92f
Kazuma Takata <neilbuzztakata@gmail.com>
2019-11-01 22:07:41 +0900

    improve reflection

--- reflection/Main.java
+++ reflection/Main.java
@@ -14,18 +14,22 @@ import java.io.*;
         Field f = cls.getField("out");        
         Object ss =  f.get(cls); 
         Method mm = ss.getClass().getMethod("println", clsstring  );        
-        mm.invoke(ss, "eee");//ss.println("eeee");
+
+        
+    
+
+        Object[] objects = new Object[3];
+        objects[0] = "foo";
+
+
+        Object obj = objects[0];
+
+
+
+        Object ob =  mm.invoke(ss, obj);//ss.println("eeee");
         
 
 
-
-
-
-        //Object[] objects = new Object[3];
-        //objects[0] = "foo";
-
-
-        //Object obj = objects[0];
 
         //Class cl = Class.forName("java.lang.String");
         //Method method = cl.getMethod()
