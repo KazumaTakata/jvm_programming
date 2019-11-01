8b033aaaae76890c033f00d73a38a96e37bb3562
Kazuma Takata <neilbuzztakata@gmail.com>
2019-11-02 00:41:46 +0900

    add minimum printout

---
+++ reflection/PrintOutTest.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class PrintOutTest {
+  public static void main(String args[]) throws IOException {
+    BufferedWriter out =
+        new BufferedWriter(
+            new OutputStreamWriter(new FileOutputStream(FileDescriptor.out), "ASCII"), 512);
+    out.write("test string");
+    out.write('\n');
+    out.flush();
+
+
+    FileOutputStream fos =  new FileOutputStream(FileDescriptor.out);
+    fos.write("few".getBytes() );
+    fos.flush();
+    fos.close();
+  }
+}
