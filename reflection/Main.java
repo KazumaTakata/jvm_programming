c5384246cf79e3c66dc2289f4fecf3df5ae34720
Kazuma Takata <neilbuzztakata@gmail.com>
2019-11-01 16:46:09 +0900

    improve reflection

--- reflection/Main.java
+++ reflection/Main.java
@@ -1,18 +1,40 @@
 import java.lang.reflect.*;
 import java.io.*;
 
-public class Main {
+ class Main {
 
     public static void main(String[] args) throws IllegalAccessException {
         try {
         String classstring = "java.lang.System";
         Class cls = Class.forName(classstring);
-        
+
+        String stringclassstring = "java.lang.String";        
+        Class clsstring = Class.forName(stringclassstring);
 
         Field f = cls.getField("out");        
-        PrintStream ss = (PrintStream) f.get(System.class); 
-        ss.println("eeee");
-        } catch (NoSuchFieldException | ClassNotFoundException eee){
+        Object ss =  f.get(cls); 
+        Method mm = ss.getClass().getMethod("println", clsstring  );        
+        mm.invoke(ss, "eee");//ss.println("eeee");
+        
+
+
+
+
+
+        //Object[] objects = new Object[3];
+        //objects[0] = "foo";
+
+
+        //Object obj = objects[0];
+
+        //Class cl = Class.forName("java.lang.String");
+        //Method method = cl.getMethod()
+
+        //cl.class str = (cl.class) obj;
+
+
+
+        } catch (InvocationTargetException | NoSuchMethodException |NoSuchFieldException | ClassNotFoundException eee){
         }
             
         } 
